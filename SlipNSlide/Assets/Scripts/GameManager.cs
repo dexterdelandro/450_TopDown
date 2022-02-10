996b9c8492d4363b1271f15cede5c004fd905616
notsummerkarnani <93565277+notsummerkarnani@users.noreply.github.com>
2022-02-10 15:32:48 -0500

    Linked score to manager

--- SlipNSlide/Assets/Scripts/GameManager.cs
+++ SlipNSlide/Assets/Scripts/GameManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    Score scoreScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreScript = gameObject.GetComponent<Score>();
     }
 
     // Update is called once per frame
