eedcc2301c73280f8c5349381fb373b9aec55a39
CQuez <crq4376@rit.edu>
2022-02-17 13:42:49 -0500

    TileMap with small room Generation
    TileMap room for looks from free online assets. 5 empty rooms are made in a line.

--- SlipNSlide/Assets/Scripts/RoomGenerator.cs
+++ SlipNSlide/Assets/Scripts/RoomGenerator.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class RoomGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject startRoom;
+    public GameObject nextRoom;
+
+    private GameObject temp;
+
+
     void Start()
     {
-        
+        temp = Instantiate(startRoom, new Vector3(0,0,0), Quaternion.identity);
+
+        CreateFive();
     }
+
+    void CreateFive()
+    {
+        Vector3 nextLoc = temp.transform.position;
+
+        for (int i = 0; i <= 5; i++)
+        {
+            nextLoc.y += 18;
+            temp = Instantiate(nextRoom, nextLoc, Quaternion.identity);
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
