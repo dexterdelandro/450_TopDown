cb020f896352ff35979964b693dcce0c4f67c0eb
CQuez <crq4376@rit.edu>
2022-03-31 17:01:28 -0400

    Room spawn fixes
    Room spawn is still unpredictable but currently removing bugs

--- SlipNSlide/Assets/Scripts/RoomTemplates.cs
+++ SlipNSlide/Assets/Scripts/RoomTemplates.cs
@@ -11,6 +11,8 @@ public class RoomTemplates : MonoBehaviour
 
     public GameObject[] LayOuts;
 
+    public GameObject blocker;
+
     // Start is called before the first frame update
     void Start()
     {
